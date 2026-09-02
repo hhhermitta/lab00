c25bce203cabc3015586303488f9265a88a4e9d4
Admin <Admin@DESKTOP-ABPT33U>
2026-09-02 19:24:09 +0300

    Change function

--- Milingert/Milingert/Milingert.cpp
+++ Milingert/Milingert/Milingert.cpp
@@ -5,7 +5,14 @@
 
 int main()
 {
-    std::cout << "Hello World!1\n";
+    std::cout << "Hello World!\n";
+    int x;
+    std::cin >> x;
+
+    if (x != 0) {
+        std::cout << 10 / x;
+        return 0;
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
